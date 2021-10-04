fc5877939d9b2405e4873c758eefe8e34ea84515
ian.liu <ian.liu@ucloud.cn>
2021-10-04 20:08:01 +0800

    x

---
+++ dataStructure/tree.js
@@ -0,0 +1,30 @@
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(key) {
+
+    }
+
+    search(key) {
+
+    }
+
+    // 中序遍历
+    inOrderTraverse() {
+
+    }
+
+    // 前序遍历
+    preOrderTraverse() {
+
+    }
+
+    // 后续
+    postOrderTraverse() {
+
+    }
+
+
+}
